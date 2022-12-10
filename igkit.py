d67bb0430d94295d04a134df66900ced001d1a9c
Sam <107519227+SamueleAmato@users.noreply.github.com>
2022-12-10 23:11:08 +0000

    Update igkit.py

--- igkit.py
+++ igkit.py
@@ -23,7 +23,7 @@ except  Exception:
           else:
                os.chdir("src")
                os.system("python3 setup.py")
-
+if systemis == "Windows":
      user_agents = [
      "Mozilla/1.22 (compatible; MSIE 10.0; Windows 3.1)",
      "Mozilla/4.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/5.0)",
