3bd7830168587ccc20b8b4ff4929f42e9892cc25
Sam <107519227+SamueleAmato@users.noreply.github.com>
2022-12-17 19:53:38 +0000

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -5,11 +5,13 @@ if scelta == "1":
     os.system("pip install requests")
     os.system("pip install mechanize")
     os.system("pip install secure-smtplib")
+    os.system("cls")
 elif scelta == "2":
     os.system("pip3 install bs4")
     os.system("pip3 install requests")
     os.system("pip3 install mechanize")
     os.system("pip3 install secure-smtplib")
+    os.system("clear")
 print("Run python smh.py to run the program")
 
 print("WARNING: vpn doesn't work on windwos and termux, also to use it on linux systems you have to install windscribe-cli; tutorial --> https://www.geeksforgeeks.org/how-to-setup-vpn-on-ubuntu-linux-system-for-ip-spoofing-using-windscribe/")
